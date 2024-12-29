e57a4e4812368c22606b94a9dc95bb6a98f7c9e4
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-12-29 18:54:08 +0100

    feat: create and style design-system-footer component

--- src/app/design-system/design-system.component.ts
+++ src/app/design-system/design-system.component.ts
@@ -1,13 +1,18 @@
-import { CommonModule } from '@angular/common';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { DesignSystemSidebarComponent } from './shared/design-system-sidebar/design-system-sidebar.component';
+import { DesignSystemFooterComponent } from './shared/design-system-footer/design-system-footer.component';
 
 @Component({
   selector: 'design-system',
   templateUrl: './design-system.component.html',
   styleUrls: ['./design-system.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, DesignSystemSidebarComponent],
+  imports: [
+    RouterOutlet,
+    DesignSystemSidebarComponent,
+    DesignSystemFooterComponent,
+  ],
 })
 export default class DesignSystemComponent {}
---
+++ src/app/design-system/shared/design-system-header/design-system-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-design-system-header',
+  imports: [],
+  templateUrl: './design-system-header.component.html',
+  styleUrl: './design-system-header.component.scss'
+})
+export class DesignSystemHeaderComponent {
+
+}
