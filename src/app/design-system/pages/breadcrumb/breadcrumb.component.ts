fe620e3009cfdb7bae2a20a272b38defca05408d
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 18:23:24 +0100

    feat: Create and document ds-breadcrumb component

--- src/app/design-system/pages/breadcrumb/breadcrumb.component.ts
+++ src/app/design-system/pages/breadcrumb/breadcrumb.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DsBreadcrumbComponent } from '@design-system/components/ds-breadcrumb/ds-breadcrumb.component';
 
 @Component({
   selector: 'breadcrumb',
-  imports: [],
+  imports: [DsBreadcrumbComponent],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class BreadcrumbComponent {}
