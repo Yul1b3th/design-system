40329ba3fb1636481a49ce2383c1e7fd2a5743b8
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 19:43:13 +0100

    feat: Create and document ds-modal component

--- src/app/design-system/pages/modal/modal.component.ts
+++ src/app/design-system/pages/modal/modal.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DsModalComponent } from '@design-system/components/ds-modal/ds-modal.component';
+import { DsButtonComponent } from '@design-system/components/ds-button/ds-button.component';
 
 @Component({
   selector: 'modal',
-  imports: [],
+  imports: [DsModalComponent, DsButtonComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ModalComponent {}
+export default class ModalComponent {
+  isModalOpen = false;
+
+  handleClose() {
+    this.isModalOpen = false;
+  }
+
+  handleSave() {
+    console.log('Modal saved');
+    this.isModalOpen = false;
+  }
+}
