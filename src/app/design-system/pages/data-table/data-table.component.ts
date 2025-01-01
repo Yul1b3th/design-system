00cc3aa59d8fa5fa350caaac715a3896e3a632df
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 19:18:42 +0100

    feat: Create and document ds-data-table component

--- src/app/design-system/pages/data-table/data-table.component.ts
+++ src/app/design-system/pages/data-table/data-table.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DsDataTableComponent } from '@design-system/components/ds-data-table/ds-data-table.component';
 
 @Component({
   selector: 'data-table',
-  imports: [],
+  imports: [DsDataTableComponent],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class DataTableComponent {}
