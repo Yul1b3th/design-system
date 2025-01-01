cdc339302f2a40744226eb9219731818db9aabcc
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 19:29:18 +0100

    feat: Create and document ds-dropdown component

--- src/app/design-system/components/ds-dropdown/ds-dropdown.component.ts
+++ src/app/design-system/components/ds-dropdown/ds-dropdown.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  inject,
+} from '@angular/core';
+import { ThemeService } from '@design-system/services/theme.service';
 
 @Component({
-  selector: 'app-ds-dropdown',
-  imports: [],
+  selector: 'ds-dropdown',
+  imports: [CommonModule],
   templateUrl: './ds-dropdown.component.html',
-  styleUrl: './ds-dropdown.component.scss'
+  styleUrls: ['./ds-dropdown.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DsDropdownComponent {
+  public readonly themeService = inject(ThemeService);
 
+  @Input() label: string = '';
+  @Input() options: { label: string; value: any }[] = [];
+  @Input() selectedValue: any;
+  @Input() disabled: boolean = false;
+
+  isOpen: boolean = false;
+
+  toggleDropdown(): void {
+    if (!this.disabled) {
+      this.isOpen = !this.isOpen;
+    }
+  }
+
+  selectOption(option: any): void {
+    this.selectedValue = option;
+    this.isOpen = false;
+  }
 }
--- src/app/design-system/pages/dropdown/dropdown.component.ts
+++ src/app/design-system/pages/dropdown/dropdown.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DsDropdownComponent } from '@design-system/components/ds-dropdown/ds-dropdown.component';
 
 @Component({
   selector: 'dropdown',
-  imports: [],
+  imports: [DsDropdownComponent],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class DropdownComponent {}
