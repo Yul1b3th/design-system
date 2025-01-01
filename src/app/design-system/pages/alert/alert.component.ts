0e85fbf4e3ecd7e3596871344460002ae5c9413d
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 17:41:21 +0100

    feat: Create and document ds-alert component

--- src/app/design-system/pages/alert/alert.component.ts
+++ src/app/design-system/pages/alert/alert.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DsAlertComponent } from '@design-system/components/ds-alert/ds-alert.component';
 
 @Component({
   selector: 'alert',
-  imports: [],
+  imports: [DsAlertComponent],
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AlertComponent {}
