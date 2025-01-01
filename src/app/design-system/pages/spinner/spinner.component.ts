68a9a54262eb88726675d3e00f311c7271d2f9b1
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 20:32:27 +0100

    feat: Create and document ds-spinner component

--- src/app/design-system/pages/spinner/spinner.component.ts
+++ src/app/design-system/pages/spinner/spinner.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DsSpinnerComponent } from '@design-system/components/ds-spinner/ds-spinner.component';
 
 @Component({
   selector: 'spinner',
-  imports: [],
+  imports: [DsSpinnerComponent],
   templateUrl: './spinner.component.html',
   styleUrl: './spinner.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class SpinnerComponent {}
