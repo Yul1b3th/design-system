72ac9825357d1ffbd88b2c1bfdf9ac5bcc6f4177
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 13:58:32 +0100

    feat: Create and document ds-slider component

--- src/app/design-system/pages/slider/slider.component.ts
+++ src/app/design-system/pages/slider/slider.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { DsSliderComponent } from '@design-system/components/ds-slider/ds-slider.component';
 
 @Component({
   selector: 'slider',
-  imports: [],
+  imports: [ReactiveFormsModule, DsSliderComponent],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SliderComponent {}
+export default class SliderComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      sliderValue: [50],
+    });
+  }
+}
