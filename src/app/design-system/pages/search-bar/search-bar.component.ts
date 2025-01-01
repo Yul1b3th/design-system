4ad81b4b47ad4812608061fff3c3ce1390ac9748
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 17:04:10 +0100

    feat: Add and document ds-search-bar component

--- src/app/design-system/pages/search-bar/search-bar.component.ts
+++ src/app/design-system/pages/search-bar/search-bar.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DsSearchBarComponent } from '@design-system/components/ds-search-bar/ds-search-bar.component';
 
 @Component({
   selector: 'search-bar',
-  imports: [],
+  imports: [ReactiveFormsModule, DsSearchBarComponent],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchBarComponent {}
+export default class SearchBarComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      searchValue: [''],
+    });
+
+    // Suscribirse a los cambios en el valor del formulario
+    this.form.get('searchValue')?.valueChanges.subscribe((value) => {
+      console.log('Valor seleccionado en el padre:', value);
+    });
+  }
+}
