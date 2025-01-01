76c1a79cd5386452404483fa9dee725109a05860
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 13:43:57 +0100

    feat: Add ds-date-picker component and documentation

--- src/app/design-system/pages/date-picker/date-picker.component.ts
+++ src/app/design-system/pages/date-picker/date-picker.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+import { DsDatePickerComponent } from '@design-system/components/ds-date-picker/ds-date-picker.component';
 
 @Component({
-  selector: 'date-picker',
-  imports: [],
+  selector: 'page-date-picker',
+  imports: [ReactiveFormsModule, DsDatePickerComponent],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class DatePickerComponent {}
+export default class DatePickerComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      datePicker: [''],
+    });
+  }
+}
