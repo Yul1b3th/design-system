45e6d44b676a6f72149bfc61ba8fd9de2a2697b1
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-01 20:01:45 +0100

    feat: Create and document ds-pagination component

--- src/app/design-system/pages/pagination/pagination.component.ts
+++ src/app/design-system/pages/pagination/pagination.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DsPaginationComponent } from '@design-system/components/ds-pagination/ds-pagination.component';
 
 @Component({
   selector: 'pagination',
-  imports: [],
+  imports: [DsPaginationComponent],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PaginationComponent {}
+export default class PaginationComponent {
+  currentPage = 1;
+
+  onPageChange(newPage: number) {
+    this.currentPage = newPage;
+  }
+}
