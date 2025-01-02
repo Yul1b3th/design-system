d0cd672b422e5f7feaae472cd4b25fcd0a7ea53f
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-02 19:39:01 +0100

    Update mobile-sidebar

--- src/app/design-system/shared/design-system-header/design-system-header.component.ts
+++ src/app/design-system/shared/design-system-header/design-system-header.component.ts
@@ -2,11 +2,14 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
-  Signal,
   signal,
+  effect,
+  ElementRef,
+  ViewChild,
 } from '@angular/core';
 import { ThemeService } from '@design-system/services/theme.service';
 import { CommonModule } from '@angular/common';
+
 import { DesignSystemSidebarComponent } from '../design-system-sidebar/design-system-sidebar.component';
 
 @Component({
@@ -19,6 +22,22 @@ import { DesignSystemSidebarComponent } from '../design-system-sidebar/design-sy
 export class DesignSystemHeaderComponent {
   public readonly themeService = inject(ThemeService);
   public isMenuOpen = signal<boolean>(false);
+
+  @ViewChild('closeButton', { static: false })
+  closeButton!: ElementRef<HTMLButtonElement>;
+
+  constructor() {
+    effect(() => {
+      if (this.isMenuOpen()) {
+        document.body.classList.add('menu-open');
+        setTimeout(() => {
+          this.closeButton.nativeElement.focus();
+        }, 0);
+      } else {
+        document.body.classList.remove('menu-open');
+      }
+    });
+  }
 
   toggleMenu() {
     this.isMenuOpen.set(!this.isMenuOpen());
