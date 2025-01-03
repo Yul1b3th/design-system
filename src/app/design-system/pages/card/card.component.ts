78f8231a6cf9d5b5e7a563c0ca7f8d387312cf74
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-03 16:50:39 +0100

    Update SideBar

--- src/app/design-system/pages/card/card.component.ts
+++ src/app/design-system/pages/card/card.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { DsButtonComponent } from '@design-system/components/ds-button/ds-button.component';
 import { DsCardComponent } from '@design-system/components/ds-card/ds-card.component';
 
 @Component({
   selector: 'card',
-  imports: [DsCardComponent, DsButtonComponent],
+  imports: [DsCardComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
