fe5d07f401628c1608b10387d4b4ddd612d1da8a
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-04 15:58:29 +0100

    Updates

--- src/app/design-system/components/ds-dropdown/ds-dropdown.component.ts
+++ src/app/design-system/components/ds-dropdown/ds-dropdown.component.ts
@@ -2,13 +2,9 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  Input,
-  Output,
-  EventEmitter,
   inject,
   OnInit,
 } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ThemeService } from '@design-system/services/theme.service';
 
 @Component({
@@ -18,43 +14,6 @@ import { ThemeService } from '@design-system/services/theme.service';
   styleUrls: ['./ds-dropdown.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DsDropdownComponent implements OnInit {
+export class DsDropdownComponent {
   public readonly themeService = inject(ThemeService);
-  private readonly sanitizer = inject(DomSanitizer);
-
-  @Input() label: string = '';
-  @Input() options: { label: string; value: any; icon?: string }[] = [];
-  @Input() selectedValue: any;
-  @Input() disabled: boolean = false;
-  @Input() showIconsOnly: boolean = false;
-  @Input() additionalClass: string = '';
-  @Input() dropdownPadding: string = '1rem';
-  @Input() dropdownBorderRadius: string = '4px';
-  @Input() dropdownBackground: string = '#fff';
-  @Input() dropdownColor: string = '#000';
-  @Input() dropdownBorder: string = '1px solid #ccc';
-  @Output() selectedValueChange = new EventEmitter<any>();
-
-  isOpen: boolean = false;
-
-  ngOnInit(): void {
-    console.log(this.label);
-    console.log(this.options);
-  }
-
-  toggleDropdown(): void {
-    if (!this.disabled) {
-      this.isOpen = !this.isOpen;
-    }
-  }
-
-  selectOption(option: any): void {
-    this.selectedValue = option.value;
-    this.selectedValueChange.emit(option.value);
-    this.isOpen = false;
-  }
-
-  sanitizeHtml(html: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(html);
-  }
 }
