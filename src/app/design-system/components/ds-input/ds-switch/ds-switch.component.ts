efe03a9af0e733ff2e5e2f5f9044ee1945c0e961
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-05 11:39:39 +0100

    Updated ds-switch and ds-theme-switch components to properly render icons and improve styling

--- src/app/design-system/components/ds-input/ds-switch/ds-switch.component.ts
+++ src/app/design-system/components/ds-input/ds-switch/ds-switch.component.ts
@@ -2,10 +2,14 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   forwardRef,
   inject,
   Input,
   Provider,
+  Renderer2,
+  ViewChild,
+  AfterViewInit,
 } from '@angular/core';
 import {
   ControlValueAccessor,
@@ -29,8 +33,9 @@ const SWITCH_VALUE_ACCESSOR: Provider = {
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [SWITCH_VALUE_ACCESSOR],
 })
-export class DsSwitchComponent implements ControlValueAccessor {
+export class DsSwitchComponent implements ControlValueAccessor, AfterViewInit {
   public readonly themeService = inject(ThemeService);
+  private readonly renderer = inject(Renderer2);
 
   @Input() id: string = '';
   @Input() label: string = '';
@@ -38,6 +43,12 @@ export class DsSwitchComponent implements ControlValueAccessor {
   @Input() disabled: boolean = false;
   @Input() value: boolean = false;
   @Input() errorMessage: string | null = null;
+  @Input() iconOn: string | null = null;
+  @Input() iconOff: string | null = null;
+
+  @ViewChild('iconOnContainer', { static: false }) iconOnContainer!: ElementRef;
+  @ViewChild('iconOffContainer', { static: false })
+  iconOffContainer!: ElementRef;
 
   private onChange: (value: boolean) => void = () => {};
   private onTouched: () => void = () => {};
@@ -65,5 +76,14 @@ export class DsSwitchComponent implements ControlValueAccessor {
 
   handleBlur(): void {
     this.onTouched();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.iconOn && this.iconOnContainer) {
+      this.iconOnContainer.nativeElement.innerHTML = this.iconOn;
+    }
+    if (this.iconOff && this.iconOffContainer) {
+      this.iconOffContainer.nativeElement.innerHTML = this.iconOff;
+    }
   }
 }
