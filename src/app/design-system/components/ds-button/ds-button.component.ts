f65cc7e20df6d7c1746895a70422bd0f0ac2ab3a
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-06 20:22:22 +0100

    Update buttons

--- src/app/design-system/components/ds-button/ds-button.component.ts
+++ src/app/design-system/components/ds-button/ds-button.component.ts
@@ -31,10 +31,16 @@ export class DsButtonComponent implements AfterViewInit {
   @Input() disabled: boolean = false;
   @Input() ariaLabel: string = '';
   @Input() iconSvg: string | null = null;
+  @Input() btnIcon: string | null = null; // Clase para el icono
 
   // Nuevas entradas para personalización
-  @Input() bgColor: string | null = null;
   @Input() textColor: string | null = null;
+  @Input() bgColor: string | null = null;
+  @Input() padding: string | null = null;
+  @Input() fontSize: string | null = null;
+  @Input() fontWeight: string | null = null;
+  @Input() lineHeight: string | null = null;
+  @Input() border: string | null = null;
   @Input() borderRadius: string | null = null;
 
   @ViewChild('buttonElement', { static: false }) buttonElement!: ElementRef;
@@ -60,7 +66,17 @@ export class DsButtonComponent implements AfterViewInit {
       this.size,
       themeClass,
       this.block ? 'btn-block' : '',
-      this.bgColor || this.textColor || this.borderRadius ? 'btn-custom' : '',
+      this.btnIcon ? this.btnIcon : '', // Solo incluir btnIcon si se ha proporcionado
+      this.textColor ||
+      this.bgColor ||
+      this.padding ||
+      this.fontSize ||
+      this.fontWeight ||
+      this.lineHeight ||
+      this.border ||
+      this.borderRadius
+        ? 'btn-custom'
+        : '',
     ].filter(Boolean);
   }
 
@@ -72,6 +88,21 @@ export class DsButtonComponent implements AfterViewInit {
     }
     if (this.textColor) {
       this.renderer.setStyle(buttonElement, 'color', this.textColor);
+    }
+    if (this.padding) {
+      this.renderer.setStyle(buttonElement, 'padding', this.padding);
+    }
+    if (this.fontSize) {
+      this.renderer.setStyle(buttonElement, 'font-size', this.fontSize);
+    }
+    if (this.fontWeight) {
+      this.renderer.setStyle(buttonElement, 'font-weight', this.fontWeight);
+    }
+    if (this.lineHeight) {
+      this.renderer.setStyle(buttonElement, 'line-height', this.lineHeight);
+    }
+    if (this.border) {
+      this.renderer.setStyle(buttonElement, 'border', this.border);
     }
     if (this.borderRadius) {
       this.renderer.setStyle(buttonElement, 'border-radius', this.borderRadius);
