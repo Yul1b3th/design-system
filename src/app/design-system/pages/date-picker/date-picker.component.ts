5d6515f65bd0ef5bd7262ba371265a60a993fd3e
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-11 18:59:38 +0100

    Update page-date-picker

--- src/app/design-system/pages/date-picker/date-picker.component.ts
+++ src/app/design-system/pages/date-picker/date-picker.component.ts
@@ -5,18 +5,20 @@ import {
   OnInit,
 } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
 
 import { DsDatePickerComponent } from '@design-system/components/ds-date-picker/ds-date-picker.component';
+import { ThemeService } from '@design-system/services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'page-date-picker',
-  imports: [ReactiveFormsModule, DsDatePickerComponent],
+  imports: [CommonModule, ReactiveFormsModule, DsDatePickerComponent],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class DatePickerComponent implements OnInit {
+  public readonly themeService = inject(ThemeService);
   private readonly fb = inject(FormBuilder);
   form!: FormGroup;
 
