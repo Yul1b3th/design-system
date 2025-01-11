11d89f77865f3610a13f18f0ce05c39bdbfcedc0
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-11 11:55:17 +0100

    Update ds-card-skeleton

--- src/app/design-system/pages/card/card-skeleton/card-skeleton.component.ts
+++ src/app/design-system/pages/card/card-skeleton/card-skeleton.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DsCardSkeletonComponent } from '@design-system/components/ds-card/ds-card-skeleton/ds-card-skeleton.component';
+import { ThemeService } from '@design-system/services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'card-skeleton',
-  imports: [DsCardSkeletonComponent],
+  imports: [CommonModule, DsCardSkeletonComponent],
   templateUrl: './card-skeleton.component.html',
   styleUrl: './card-skeleton.component.scss',
 })
-export default class CardSkeletonComponent {}
+export default class CardSkeletonComponent {
+  public readonly themeService = inject(ThemeService);
+}
