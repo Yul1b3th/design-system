db94f6110dbd9b9e02c115592bfcd2322972827b
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-16 23:21:25 +0100

    Update ds-alert

--- src/app/design-system/components/ds-alert/ds-alert.component.ts
+++ src/app/design-system/components/ds-alert/ds-alert.component.ts
@@ -5,6 +5,8 @@ import {
   Input,
   inject,
 } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 import { ThemeService } from '@design-system/services/theme.service';
 
 @Component({
@@ -16,6 +18,7 @@ import { ThemeService } from '@design-system/services/theme.service';
 })
 export class DsAlertComponent {
   public readonly themeService = inject(ThemeService);
+  private readonly sanitizer = inject(DomSanitizer);
 
   @Input() type: 'success' | 'error' | 'warning' | 'info' = 'info';
   @Input() message: string = '';
@@ -25,5 +28,34 @@ export class DsAlertComponent {
 
   dismiss(): void {
     this.isVisible = false;
+  }
+
+  getIcon(): SafeHtml {
+    let iconSvg: string;
+    switch (this.type) {
+      case 'success':
+        iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                   </svg>`;
+        break;
+      case 'error':
+        iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                   </svg>`;
+        break;
+      case 'warning':
+        iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                   </svg>`;
+        break;
+      case 'info':
+        iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                   </svg>`;
+        break;
+      default:
+        iconSvg = '';
+    }
+    return this.sanitizer.bypassSecurityTrustHtml(iconSvg);
   }
 }
